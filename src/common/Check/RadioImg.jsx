5e4c603751f74cfd97d3dd76cfffac3d5a6d3f61
seokuuu <wazzagoo10@gmail.com>
2023-08-10 14:27:10 +0900

    라디오 이미지 추가

---
+++ src/common/Check/RadioImg.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { CheckBox } from './Checkbox';
+
+export const RadioMainDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const RadioCircleDiv = styled.div`
+  background-color: ${({ isChecked }) => (isChecked ? '#4c83d6' : '#cedcf2')};
+  border-radius: 100%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RadioInnerCircleDiv = styled.div`
+  background-color: #fff;
+  border-radius: 100%;
+  width: 10px;
+  height: 10px;
+`;
+
+// const [checkRadio, setCheckRadio] = useState(
+//   Array.from({ length: 3 }, () => false)
+// ); // 라디오 박스
+
+// const [selectedRadio, setSelectedRadio] = useState('');
