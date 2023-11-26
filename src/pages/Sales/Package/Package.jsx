d6da85fec64969b017cbb2d3636613797ed9ba52
seokuuu <114736988+seokuuu@users.noreply.github.com>
2023-11-26 23:53:12 +0900

    [feat] : 버튼 추가 수정

--- src/pages/Sales/Package/Package.jsx
+++ src/pages/Sales/Package/Package.jsx
@@ -105,7 +105,7 @@ const Package = ({}) => {
                   <PartWrap first>
                     <h6>규격 약호</h6>
                     <Input />
-                    <GreyBtn style={{ width: '70px' }} height={35} margin={10} onClick={modalOpen}>
+                    <GreyBtn style={{ width: '70px' }} height={35} margin={10} fontSize={17} onClick={modalOpen}>
                       찾기
                     </GreyBtn>
                   </PartWrap>
