5ac39c37d3db240dc9b7febb79687254138f394f
JHKIMS <wjdgns5131@gmail.com>
2024-01-05 10:22:26 +0900

    fix: 회원가입 작업중-500에러

--- src/modal/Common/TestPopup.jsx
+++ src/modal/Common/TestPopup.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AlertPopup from '../Alert/AlertPopup'
-import { popupMessages } from '../Alert/PopupMessages'
+import { useAtom } from 'jotai'
+import { popupObject } from '../../store/Layout/Layout'
 
 const TestPopup = () => {
-  const nextPopupHandler = () => {
-    
+  const [popupSwitch, setPopupSwitch] = useState(false)
+  const [, setNowPopup] = useAtom(popupObject) // Jotai를 사용하여 상태 관리
+
+  // 팝업을 열 때 초기 상태 설정
+  const openPopup = () => {
+    setNowPopup({
+      num: '1-팝업',
+      title: '팝업 제목',
+      content: '팝업 내용',
+      next: '2-팝업',
+    })
+    setPopupSwitch(true)
   }
 
   return (
-    <>
-      <AlertPopup />
-    </>
+    <div>
+      <button onClick={openPopup}>팝업 열기</button>
+      {popupSwitch && <AlertPopup setPopupSwitch={setPopupSwitch} />}
+    </div>
   )
 }
 
---
+++ src/pages/User/SignUp/SignUpAgreeModal.jsx
@@ -0,0 +1,5 @@
+const SignUpAgreeModal = () => {
+  return <div>Test</div>
+}
+
+export default SignUpAgreeModal
