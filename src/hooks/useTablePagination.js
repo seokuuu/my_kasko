68015688a94d292686e8220ca4eebd1a229a558e
Jin Park <jinpark1dev@gmail.com>
2024-01-15 16:24:45 +0900

    Update useTablePagination.js to include table in the description

--- src/hooks/useTablePagination.js
+++ src/hooks/useTablePagination.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react'
 
 /**
  * @description
- * 페이지네이션값과 핸들러를 반환해줍니다.
+ * 테이블 페이지네이션값과 핸들러를 반환해줍니다.
  * @param data 테이블 목록 데이터를 넣어주시면 됩니다.
  * @param setState 유동적으로 변경되는 검색 필터 관련 setState를 넣어주시면 됩니다.
  */
