d14f572fea38685ea26b7435cd0364d2c277e79e
김철준 <jjalseu@gimcheoljun-ui-MacBookPro.local>
2024-01-17 11:03:31 +0900

    feat : 관리자 > 운영 관리 > 행 색깔 변경 & 창고 관리 상세 주소 필드 제거

--- src/constants/admin/popup.js
+++ src/constants/admin/popup.js
@@ -6,21 +6,21 @@ import { checkboxSelection, headerCheckboxSelection } from '../../pages/Table/ut
 
 // 팝업 목록 헤더
 export const PopupListFieldCols = [
-  { field: '', checkboxSelection, headerCheckboxSelection, maxWidth: 50 },
-  { field: '순번', maxWidth: 100 },
-  { field: '제목' },
-  { field: '노출 유/무', maxWidth: 150 },
-  { field: '작성자', maxWidth: 150 },
-  { field: '노출기간', maxWidth: 200 },
+	{ field: '', checkboxSelection, headerCheckboxSelection, maxWidth: 50, headerClass: 'custom-header-style' },
+	{ field: '순번', maxWidth: 100, headerClass: 'custom-header-style' },
+	{ field: '제목', minWidth: 300, headerClass: 'custom-header-style' },
+	{ field: '노출 유/무', maxWidth: 200, headerClass: 'custom-header-style' },
+	{ field: '작성자', minWidth: 450, headerClass: 'custom-header-style' },
+	{ field: '노출기간', minWidth: 400, headerClass: 'custom-header-style' },
 ]
 
 // 팝업 목록 키값 맵핑
 export const PopupListFields = {
-  카테고리: 'category',
-  제목: 'title',
-  노출기간: 'date',
-  '노출 유/무': 'status',
-  작성자: 'name',
-  순번: 'id',
-  고유값: 'uid',
+	카테고리: 'category',
+	제목: 'title',
+	노출기간: 'date',
+	'노출 유/무': 'status',
+	작성자: 'name',
+	순번: 'id',
+	고유값: 'uid',
 }
--- src/pages/Operate/Operation/Operation/components/ProductRange.jsx
+++ src/pages/Operate/Operation/Operation/components/ProductRange.jsx
@@ -1,19 +1,9 @@
 import { useAtom, useAtomValue, useSetAtom } from 'jotai'
-import React, { useEffect, useMemo, useState } from 'react'
-import {
-	useProductRangeDetailsQuery,
-	useProductRangeListQuery,
-	useProductRangeRegisterMutation,
-	useProductRangeRemoveMutation,
-	useProductRangeUpdateMutation,
-} from '../../../../../api/operate/productRange'
-import { ProductRangeFieldCols, ProductRangeFields } from '../../../../../constants/admin/ProductRange'
-import { add_element_field } from '../../../../../lib/tableHelpers'
+import React, { useEffect, useMemo } from 'react'
+import { ProductRangeFieldCols } from '../../../../../constants/admin/ProductRange'
 import { TableContianer } from '../../../../../modal/External/ExternalFilter'
 import AddProduct from '../../../../../modal/Operate/AddProduct'
 import {
-	btnCellUidAtom,
-	doubleClickedRowAtom,
 	operateAddAtom,
 	popupAtom,
 	popupObject,
@@ -22,47 +12,45 @@ import {
 } from '../../../../../store/Layout/Layout'
 import Table from '../../../../Table/Table'
 import CommonTableHeader from '../../../UI/CommonTableHeader'
-import { commonListSearchInitValue } from '../../../constants'
-import useTablePaginationPageChange from '../../../../../hooks/useTablePaginationPageChange'
-import useProductRangeList from '../../../hook/useProductRangeList'
 import useProductRange from '../../../hook/useProductRange'
+import useProductRangeList from '../../../hook/useProductRangeList'
 
 /**
  * @description
  * 제품군 관리
  */
 const ProductRange = () => {
-  // 목록 관련 데이터 훅입니다.
-  const { mappingData, rows, refetch, isLoading, setSearch, pagination, onPageChanage } = useProductRangeList()
-  // 수정,등록,삭제,상세 관련 데이터 훅입니다.
-  const { remove, removeData, detailsData, onSpartChange, initUid, onDetermineFunction } = useProductRange()
-  // 모달
-  const [modal, setModal] = useAtom(operateAddAtom)
-  // 팝업 모달 여닫이 여부 & 팝업 타입 설정(보내는 값에 따라 팝업 내용이 달라짐.)
-  const [popupSwitch, setPopupSwitch] = useAtom(popupAtom)
-  const setNowPopupType = useSetAtom(popupTypeAtom) // 팝업 타입 2(확인 취소 버튼이 있는 모달)
-  const setNowPopup = useSetAtom(popupObject) // 팝업 객체
-  // 테이블에서 선택된 값
-  const selected = useAtomValue(selectedRowsAtom)
-  // 선택된 데이터 갯수
-  const selectedLength = useMemo(() => (selected ? selected.length : 0), [selected])
+	// 목록 관련 데이터 훅입니다.
+	const { mappingData, rows, refetch, isLoading, setSearch, pagination, onPageChanage } = useProductRangeList()
+	// 수정,등록,삭제,상세 관련 데이터 훅입니다.
+	const { remove, removeData, detailsData, onSpartChange, initUid, onDetermineFunction } = useProductRange()
+	// 모달
+	const [modal, setModal] = useAtom(operateAddAtom)
+	// 팝업 모달 여닫이 여부 & 팝업 타입 설정(보내는 값에 따라 팝업 내용이 달라짐.)
+	const [popupSwitch, setPopupSwitch] = useAtom(popupAtom)
+	const setNowPopupType = useSetAtom(popupTypeAtom) // 팝업 타입 2(확인 취소 버튼이 있는 모달)
+	const setNowPopup = useSetAtom(popupObject) // 팝업 객체
+	// 테이블에서 선택된 값
+	const selected = useAtomValue(selectedRowsAtom)
+	// 선택된 데이터 갯수
+	const selectedLength = useMemo(() => (selected ? selected.length : 0), [selected])
 
-  // 삭제 핸들러
-  function removeEventHandler() {
-    if (!selectedLength && selectedLength === 0) return alert('삭제할 목록을 선택해주세요.')
-    console.log('removeData :', removeData)
-    /**
-     * @description
-     * 모달 관련 섦명
-     * setNowPopup에 넘겨주는 객체의 num의 첫번째 숫자 관련한 모달이 나타납니다.
-     * 확인을 누를시 ( 2번의 경우),next값이 있는 경우, fun 함수가 실행되고 next 값의 첫번째 숫자에 대한 모달이 나타납니다.
-     * 취소를 누를시 ,모달이 닫힙니다.
-     * @todo
-     * 확인을 누를시, 분기처리
-     * removeData 값 여부에 따라 어떤 모달 넘버를 넘겨줄지에 대한 분기 처리
-     *
-     *
-     */
+	// 삭제 핸들러
+	function removeEventHandler() {
+		if (!selectedLength && selectedLength === 0) return alert('삭제할 목록을 선택해주세요.')
+		console.log('removeData :', removeData)
+		/**
+		 * @description
+		 * 모달 관련 섦명
+		 * setNowPopup에 넘겨주는 객체의 num의 첫번째 숫자 관련한 모달이 나타납니다.
+		 * 확인을 누를시 ( 2번의 경우),next값이 있는 경우, fun 함수가 실행되고 next 값의 첫번째 숫자에 대한 모달이 나타납니다.
+		 * 취소를 누를시 ,모달이 닫힙니다.
+		 * @todo
+		 * 확인을 누를시, 분기처리
+		 * removeData 값 여부에 따라 어떤 모달 넘버를 넘겨줄지에 대한 분기 처리
+		 *
+		 *
+		 */
 
 		setPopupSwitch(true)
 		// setNowPopupType(2)
@@ -79,45 +67,45 @@ const ProductRange = () => {
 		})
 	}
 
-  // 삭제할 데이터 중 사용중인 제품군이 있다면 예외 모달을 띄워줍니다.
-  useEffect(() => {
-    if (removeData && removeData.data.data.length > 0) {
-      alert('삭제할 수 없습니다.\n해당 항목은 현재 사용 중입니다.')
-    }
-  }, [removeData])
+	// 삭제할 데이터 중 사용중인 제품군이 있다면 예외 모달을 띄워줍니다.
+	useEffect(() => {
+		if (removeData && removeData.data.data.length > 0) {
+			alert('삭제할 수 없습니다.\n해당 항목은 현재 사용 중입니다.')
+		}
+	}, [removeData])
 
-  return (
-    <TableContianer>
-      <CommonTableHeader
-        title={detailsData ? '제품군 수정' : '제품군 추가'}
-        selectedLength={selectedLength}
-        totalLength={mappingData ? mappingData.length : 0}
-        toRegister={() => setModal(true)}
-        removeEventHandler={removeEventHandler}
-        setState={setSearch}
-      />
-      <Table
-        getCol={ProductRangeFieldCols}
-        getRow={rows}
-        setChoiceComponent={() => {}}
-        tablePagination={pagination}
-        onPageChange={onPageChanage}
-        noRowsMessage="제품군 목록이 비어있습니다."
-        loading={isLoading}
-      />
-      {modal && (
-        <AddProduct
-          initValue={detailsData ? detailsData.spart : ''}
-          title={'제품군 추가'}
-          contentTitle={'제품군 입력'}
-          deliveryHandler={onSpartChange}
-          register={onDetermineFunction}
-          // 등록과 수정을 구분하기 위해 => 초기화 해주지 않으면 등록을 눌렀을 때, detailsId 값으로 인해 수정이 되는 경우가 있을 수 있습니다.
-          closeHandler={initUid}
-        />
-      )}
-    </TableContianer>
-  )
+	return (
+		<TableContianer>
+			<CommonTableHeader
+				title={detailsData ? '제품군 수정' : '제품군 추가'}
+				selectedLength={selectedLength}
+				totalLength={mappingData ? mappingData.length : 0}
+				toRegister={() => setModal(true)}
+				removeEventHandler={removeEventHandler}
+				setState={setSearch}
+			/>
+			<Table
+				getCol={ProductRangeFieldCols}
+				getRow={rows}
+				setChoiceComponent={() => {}}
+				tablePagination={pagination}
+				onPageChange={onPageChanage}
+				noRowsMessage="제품군 목록이 비어있습니다."
+				loading={isLoading}
+			/>
+			{modal && (
+				<AddProduct
+					initValue={detailsData ? detailsData.spart : ''}
+					title={'제품군 추가'}
+					contentTitle={'제품군 입력'}
+					deliveryHandler={onSpartChange}
+					register={onDetermineFunction}
+					// 등록과 수정을 구분하기 위해 => 초기화 해주지 않으면 등록을 눌렀을 때, detailsId 값으로 인해 수정이 되는 경우가 있을 수 있습니다.
+					closeHandler={initUid}
+				/>
+			)}
+		</TableContianer>
+	)
 }
 
 export default ProductRange
