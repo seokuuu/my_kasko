60b7e448b0a0a0ad76106175a3f6bc8beb123f11
KimHyuk97 <wgilooy97@naver.com>
2024-01-19 11:32:18 +0900

    공통 얼렛 컴퍼넌트

--- src/index.js
+++ src/index.js
@@ -8,16 +8,18 @@ import { QueryClientProvider } from '@tanstack/react-query'
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from './api/query'
+import AlertComponent from './store/Alert/AlertComponent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <>
-    <GlobalStyle />
-    <ThemeProvider theme={KaskoTheme}>
-      <QueryClientProvider client={queryClient}>
-        <Router />
-        <ReactQueryDevtools position="top-right" initialIsOpen={false} />
-      </QueryClientProvider>
-    </ThemeProvider>
-  </>,
+	<>
+		<GlobalStyle />
+		<ThemeProvider theme={KaskoTheme}>
+			<QueryClientProvider client={queryClient}>
+				<Router />
+				<AlertComponent />
+				<ReactQueryDevtools position="top-right" initialIsOpen={false} />
+			</QueryClientProvider>
+		</ThemeProvider>
+	</>,
 )
