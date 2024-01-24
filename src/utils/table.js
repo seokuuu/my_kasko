e3e5e62f7d3dd623ac1bbffe660273d7918e0580
joe <zh.cecon@gmail.com>
2024-01-24 15:18:07 +0900

    fix: getNormalTableRows

--- src/utils/table.js
+++ src/utils/table.js
@@ -4,6 +4,7 @@
  * @returns {object[]} 
  * - 빈 값이 '-'로 대체된 object 배열
  * - 셀 스타일 중앙으로 지정
+ * - 셀 최소너비 100으로 지정
  */
 export function getNormalTableRows(tableRows = []) {
   return tableRows.map(v => ({
