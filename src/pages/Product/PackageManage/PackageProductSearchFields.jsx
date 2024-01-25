fd89956c74e858ab1f4ac7295e39730815a9e93d
SungJin Yang <sungjinahim@gmail.com>
2024-01-25 10:24:32 +0900

    fix: 킬로그램 sum 수정

--- src/pages/Product/PackageManage/PackageProductSearchFields.jsx
+++ src/pages/Product/PackageManage/PackageProductSearchFields.jsx
@@ -43,178 +43,176 @@ const PackageProductSearchFields = ({
 
 	return (
 		<>
-			<FilterSubcontianer>
-				<FilterLeft>
-					<RowWrap>
-						<PartWrap>
-							<h6>창고 구분</h6>
-							<PWRight>
-								<MainSelect
-									options={storageList}
-									// defaultValue={storageList[0]}
-									value={search.storage}
-									name="storage"
-									onChange={(e) => commonDropdownButtonHandler(e, 'storage')}
-								/>
-							</PWRight>
-						</PartWrap>
-						{/* 매입처 */}
-						<PartWrap>
-							<h6>매입처</h6>
-							<PWRight>
-								<MainSelect
-									options={supplierList}
-									defaultValue={supplierList[0]}
-									value={search.supplier}
-									name="supplier"
-									onChange={(e) => commonDropdownButtonHandler(e, 'supplier')}
-								/>
-							</PWRight>
-						</PartWrap>
-						<PartWrap>
-							<h6>유찰 횟수</h6>
-							<ExInputsWrap>
-								<Input /> <Tilde>~</Tilde>
-								<Input />
-							</ExInputsWrap>
-						</PartWrap>
-					</RowWrap>
-					<RowWrap>
-						{/* 구분 */}
-						<PartWrap>
-							<h6>구분</h6>
-							{/* 제품군 */}
-							<PWRight>
-								<MainSelect
-									options={spartList}
-									defaultValue={spartList[0]}
-									value={search.spart}
-									name="spart"
-									onChange={(e) => commonDropdownButtonHandler(e, 'spart')}
-								/>
-							</PWRight>
-							{/* 제조사 */}
-							<PWRight>
-								<MainSelect
-									options={makerList}
-									defaultValue={makerList[0]}
-									value={search.maker}
-									name="maker"
-									onChange={(e) => commonDropdownButtonHandler(e, 'maker')}
-								/>
-							</PWRight>
-							{/* 재고 상태 */}
-							<PWRight>
-								<MainSelect
-									options={stockStatusList}
-									defaultValue={stockStatusList[0]}
-									value={search.stockStatus}
-									name="stockStatus"
-									onChange={(e) => commonDropdownButtonHandler(e, 'stockStatus')}
-								/>
-							</PWRight>
-							{/* 등급 */}
-							<PWRight>
-								<MainSelect
-									options={gradeList}
-									defaultValue={gradeList[0]}
-									value={search.grade}
-									name="grade"
-									onChange={(e) => commonDropdownButtonHandler(e, 'grade')}
-								/>
-							</PWRight>
-							{/* 정척여부 */}
-							<PWRight>
-								<MainSelect
-									options={preferThicknessList}
-									defaultValue={preferThicknessList[0]}
-									value={search.preferThickness}
-									name="preferThickness"
-									onChange={(e) => commonDropdownButtonHandler(e, 'preferThickness')}
-								/>
-							</PWRight>
-						</PartWrap>
-					</RowWrap>
-					<RowWrap>
-						<PartWrap first>
-							<h6>판매 구분</h6>
+			<FilterLeft>
+				<RowWrap>
+					<PartWrap>
+						<h6>창고 구분</h6>
+						<PWRight>
+							<MainSelect
+								options={storageList}
+								// defaultValue={storageList[0]}
+								value={search.storage}
+								name="storage"
+								onChange={(e) => commonDropdownButtonHandler(e, 'storage')}
+							/>
+						</PWRight>
+					</PartWrap>
+					{/* 매입처 */}
+					<PartWrap>
+						<h6>매입처</h6>
+						<PWRight>
+							<MainSelect
+								options={supplierList}
+								defaultValue={supplierList[0]}
+								value={search.supplier}
+								name="supplier"
+								onChange={(e) => commonDropdownButtonHandler(e, 'supplier')}
+							/>
+						</PWRight>
+					</PartWrap>
+					<PartWrap>
+						<h6>유찰 횟수</h6>
+						<ExInputsWrap>
+							<Input /> <Tilde>~</Tilde>
+							<Input />
+						</ExInputsWrap>
+					</PartWrap>
+				</RowWrap>
+				<RowWrap>
+					{/* 구분 */}
+					<PartWrap>
+						<h6>구분</h6>
+						{/* 제품군 */}
+						<PWRight>
+							<MainSelect
+								options={spartList}
+								defaultValue={spartList[0]}
+								value={search.spart}
+								name="spart"
+								onChange={(e) => commonDropdownButtonHandler(e, 'spart')}
+							/>
+						</PWRight>
+						{/* 제조사 */}
+						<PWRight>
+							<MainSelect
+								options={makerList}
+								defaultValue={makerList[0]}
+								value={search.maker}
+								name="maker"
+								onChange={(e) => commonDropdownButtonHandler(e, 'maker')}
+							/>
+						</PWRight>
+						{/* 재고 상태 */}
+						<PWRight>
+							<MainSelect
+								options={stockStatusList}
+								defaultValue={stockStatusList[0]}
+								value={search.stockStatus}
+								name="stockStatus"
+								onChange={(e) => commonDropdownButtonHandler(e, 'stockStatus')}
+							/>
+						</PWRight>
+						{/* 등급 */}
+						<PWRight>
+							<MainSelect
+								options={gradeList}
+								defaultValue={gradeList[0]}
+								value={search.grade}
+								name="grade"
+								onChange={(e) => commonDropdownButtonHandler(e, 'grade')}
+							/>
+						</PWRight>
+						{/* 정척여부 */}
+						<PWRight>
+							<MainSelect
+								options={preferThicknessList}
+								defaultValue={preferThicknessList[0]}
+								value={search.preferThickness}
+								name="preferThickness"
+								onChange={(e) => commonDropdownButtonHandler(e, 'preferThickness')}
+							/>
+						</PWRight>
+					</PartWrap>
+				</RowWrap>
+				<RowWrap>
+					<PartWrap first>
+						<h6>판매 구분</h6>
+						<CustomCheckBox
+							initOptions={[
+								{
+									checked: false,
+									text: '판매재',
+									value: '판매재',
+								},
+								{
+									checked: false,
+									text: '판매제외제',
+									value: '판매제외제',
+								},
+								{
+									checked: false,
+									text: '판매완료제',
+									value: '판매완료제',
+								},
+							]}
+							setState={setSearch}
+							stateKey="saleCategoryList"
+						/>
+					</PartWrap>
+					<PartWrap>
+						<h6>판매 유형</h6>
+						<ExCheckWrap>
 							<CustomCheckBox
 								initOptions={[
 									{
 										checked: false,
-										text: '판매재',
-										value: '판매재',
+										text: '경매대상재',
+										value: '경매대상재',
 									},
 									{
 										checked: false,
-										text: '판매제외제',
-										value: '판매제외제',
-									},
-									{
-										checked: false,
-										text: '판매완료제',
-										value: '판매완료제',
+										text: '상시판매 대상재',
+										value: '상시판매 대상재',
 									},
 								]}
 								setState={setSearch}
-								stateKey="saleCategoryList"
+								stateKey="saleType"
+								stateType="single"
 							/>
-						</PartWrap>
-						<PartWrap>
-							<h6>판매 유형</h6>
-							<ExCheckWrap>
-								<CustomCheckBox
-									initOptions={[
-										{
-											checked: false,
-											text: '경매대상재',
-											value: '경매대상재',
-										},
-										{
-											checked: false,
-											text: '상시판매 대상재',
-											value: '상시판매 대상재',
-										},
-									]}
-									setState={setSearch}
-									stateKey="saleType"
-									stateType="single"
-								/>
-							</ExCheckWrap>
-						</PartWrap>
-					</RowWrap>
+						</ExCheckWrap>
+					</PartWrap>
+				</RowWrap>
 
-					<RowWrap style={{ border: '0px' }}>
-						<PartWrap first>
-							<h6>판매가 유형</h6>
-							<CustomCheckBox
-								initOptions={[
-									{
-										checked: false,
-										text: '특가',
-										value: '특가',
-									},
-									{
-										checked: false,
-										text: '할인',
-										value: '할인',
-									},
-								]}
-								setState={setSearch}
-								stateKey="salePriceType"
-							/>
-						</PartWrap>
-					</RowWrap>
-				</FilterLeft>
-				<FilterRight>
-					<ProductNumber
-						initialValue={search.productNumberList}
-						setState={setSearch}
-						valueName={'productNumberList'}
-						height="100%"
-					/>
-				</FilterRight>
-			</FilterSubcontianer>
+				<RowWrap style={{ border: '0px' }}>
+					<PartWrap first>
+						<h6>판매가 유형</h6>
+						<CustomCheckBox
+							initOptions={[
+								{
+									checked: false,
+									text: '특가',
+									value: '특가',
+								},
+								{
+									checked: false,
+									text: '할인',
+									value: '할인',
+								},
+							]}
+							setState={setSearch}
+							stateKey="salePriceType"
+						/>
+					</PartWrap>
+				</RowWrap>
+			</FilterLeft>
+			<FilterRight>
+				<ProductNumber
+					initialValue={search.productNumberList}
+					setState={setSearch}
+					valueName={'productNumberList'}
+					height="100%"
+				/>
+			</FilterRight>
 		</>
 	)
 }
