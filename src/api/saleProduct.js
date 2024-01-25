8c6b4b34e5c0c2cf6ab063f46964277d7254d214
Jin Park <jinpark1dev@gmail.com>
2024-01-25 16:14:03 +0900

    feat: 상시 판매 관리 > 주문 확인 & 주문 확인 상세 (진행중)

--- src/api/saleProduct.js
+++ src/api/saleProduct.js
@@ -1,8 +1,12 @@
 import { client } from '.'
 import qs from 'qs'
+import useAlert from '../store/Alert/useAlert'
+import { useMutation } from '@tanstack/react-query'
+import { queryClient } from './query'
 
 const saleProductEndpoint = '/sale-product/order'
 const saleProductDetailsEndpoint = '/sale-product/order/details'
+const saleProductOrderConfirmEndpoint = '/sale-product/order/confirm'
 
 export const getSaleProductList = async (data) => {
 	return client.get(saleProductEndpoint, {
@@ -15,4 +19,21 @@ export const getSaleProductList = async (data) => {
 
 export const getSaleProductDetail = async (data) => {
 	return client.get(saleProductDetailsEndpoint, { params: data })
+}
+
+export const usePostSaleProductOrderConfirm = () => {
+	const { simpleAlert } = useAlert()
+
+	return useMutation({
+		mutationFn: async (orderParam) => {
+			await client.post(saleProductOrderConfirmEndpoint, orderParam)
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: 'cart' })
+			return simpleAlert('부분 입금을 완료하였습니다.')
+		},
+		onError: (error) => {
+			return simpleAlert(error?.data?.message || '요청중 오류가 발생했습니다.\n다시 시도해 주세요.')
+		},
+	})
 }
