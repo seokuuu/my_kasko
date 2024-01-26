01a198530b08be871b223b55ff683e8c163aacea
Jin Park <jinpark1dev@gmail.com>
2024-01-26 14:39:28 +0900

    feature: 상시 판매 관리 (진행중)

--- src/modal/Multi/SalesPackage.jsx
+++ src/modal/Multi/SalesPackage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
 import {
-  NonFadeOverlay,
-  ModalContainer,
-  WhiteCloseBtn,
-  BlueSubContainer,
-  BlueBarHeader,
-  BlueMainDiv,
-  BlueSubDiv,
-  BlueBtnWrap,
-  BlueBlackBtn,
+	NonFadeOverlay,
+	ModalContainer,
+	WhiteCloseBtn,
+	BlueSubContainer,
+	BlueBarHeader,
+	BlueMainDiv,
+	BlueSubDiv,
+	BlueBtnWrap,
+	BlueBlackBtn,
 } from '../Common/Common.Styled'
 
-import { blueModalAtom } from '../../store/Layout/Layout'
+import { blueModalAtom, salesPackageModal } from '../../store/Layout/Layout'
 import { useAtom } from 'jotai'
 
 import { ExRadioWrap } from '../External/ExternalFilter'
@@ -21,60 +21,60 @@ import { RadioMainDiv, RadioCircleDiv, RadioInnerCircleDiv } from '../../common/
 
 import { CheckBox } from '../../common/Check/Checkbox'
 
-const SalesPackage = () => {
-  const [isModal, setIsModal] = useAtom(blueModalAtom)
+const SalesPackage = ({ onClick }) => {
+	const [isModal, setIsModal] = useAtom(salesPackageModal)
 
-  const modalClose = () => {
-    setIsModal(false)
-  }
+	const modalClose = () => {
+		setIsModal(false)
+	}
 
-  const radioDummy = ['노출 ', '비노출']
+	const radioDummy = ['노출 ', '비노출']
 
-  const [checkRadio, setCheckRadio] = useState(Array.from({ length: radioDummy.length }, (_, index) => index === 0))
+	const [checkRadio, setCheckRadio] = useState(Array.from({ length: radioDummy.length }, (_, index) => index === 0))
 
-  console.log('checkRadio =>', checkRadio)
+	console.log('checkRadio =>', checkRadio)
 
-  return (
-    // 재고 관리 - 판매 구분 변경
-    <>
-      <NonFadeOverlay />
-      <ModalContainer width={530}>
-        <BlueBarHeader>
-          <div>노출 상태 변경</div>
-          <div>
-            <WhiteCloseBtn onClick={modalClose} src="/svg/white_btn_close.svg" />
-          </div>
-        </BlueBarHeader>
-        <BlueSubContainer>
-          <div>
-            <BlueMainDiv>
-              <BlueSubDiv>
-                <h6>노출 여부</h6>
-                <BlueRadioWrap style={{ gap: '50px' }}>
-                  {radioDummy.map((text, index) => (
-                    <RadioMainDiv key={index}>
-                      <RadioCircleDiv
-                        isChecked={checkRadio[index]}
-                        onClick={() => {
-                          setCheckRadio(CheckBox(checkRadio, checkRadio.length, index))
-                        }}
-                      >
-                        <RadioInnerCircleDiv isChecked={checkRadio[index]} />
-                      </RadioCircleDiv>
-                      <div style={{ display: 'flex', marginLeft: '5px', color: 'black' }}>{text}</div>
-                    </RadioMainDiv>
-                  ))}
-                </BlueRadioWrap>
-              </BlueSubDiv>
-            </BlueMainDiv>
-          </div>
-          <BlueBtnWrap>
-            <BlueBlackBtn>저장</BlueBlackBtn>
-          </BlueBtnWrap>
-        </BlueSubContainer>
-      </ModalContainer>
-    </>
-  )
+	return (
+		// 재고 관리 - 판매 구분 변경
+		<>
+			<NonFadeOverlay />
+			<ModalContainer width={530}>
+				<BlueBarHeader>
+					<div>노출 상태 변경</div>
+					<div>
+						<WhiteCloseBtn onClick={modalClose} src="/svg/white_btn_close.svg" />
+					</div>
+				</BlueBarHeader>
+				<BlueSubContainer>
+					<div>
+						<BlueMainDiv>
+							<BlueSubDiv>
+								<h6>노출 여부</h6>
+								<BlueRadioWrap style={{ gap: '50px' }}>
+									{radioDummy.map((text, index) => (
+										<RadioMainDiv key={index}>
+											<RadioCircleDiv
+												isChecked={checkRadio[index]}
+												onClick={() => {
+													setCheckRadio(CheckBox(checkRadio, checkRadio.length, index))
+												}}
+											>
+												<RadioInnerCircleDiv isChecked={checkRadio[index]} />
+											</RadioCircleDiv>
+											<div style={{ display: 'flex', marginLeft: '5px', color: 'black' }}>{text}</div>
+										</RadioMainDiv>
+									))}
+								</BlueRadioWrap>
+							</BlueSubDiv>
+						</BlueMainDiv>
+					</div>
+					<BlueBtnWrap>
+						<BlueBlackBtn onClick={onClick}>확인</BlueBlackBtn>
+					</BlueBtnWrap>
+				</BlueSubContainer>
+			</ModalContainer>
+		</>
+	)
 }
 
 export default SalesPackage
