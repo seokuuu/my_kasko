5d4b13b81aefdb742b28a2baca4bde98c75cbcaf
KimHyuk97 <wgilooy97@naver.com>
2024-01-30 17:32:54 +0900

    주문관리 API 테스트 진행 중

--- src/api/detailOrderList.js
+++ src/api/detailOrderList.js
@@ -1,21 +1,48 @@
 import { client } from './index'
 
 const urls = {
-  cancleOrderList: '/admin/order/cancel-all',
-  depositCancleOrderList: '/admin/order/deposit-all'
+	cancelOrder: '/admin/order/cancel',
+	cancelOrderList: '/admin/order/cancel-all',
+	depositCancelOrder: '/admin/order/deposit',
+	depositCancelOrderList: '/admin/order/deposit-all',
 }
+
+// 부분 주문 취소
+export async function cancelOrderPost(data) {
+	try {
+		const response = await client.post(`${urls.cancelOrder}`, data)
+		return response.data
+	} catch (e) {
+		throw new Error('주문 취소 중 오류가 발생했습니다.')
+	}
+}
+
+// 전체 주문 취소
 export async function cancelOrderList(data) {
-  try{
-    const response = await client.post(`${urls.cancleOrderList}`,data)
-    return response.data
-  }catch(e){
-    throw new Error('주문 취소 중 오류가 발생했습니다.');
-  }
-}export async function depositCancleOrderList(data) {
-  try{
-    const response = await client.post(`${urls.depositCancleOrderList}`,data)
-    return response.data
-  }catch(e){
-    throw new Error('입금 취소 중 오류가 발생했습니다.');
-  }
+	try {
+		const response = await client.post(`${urls.cancelOrderList}`, data)
+		return response.data
+	} catch (e) {
+		throw new Error('주문 취소 중 오류가 발생했습니다.')
+	}
+}
+
+// 부분 입금 취소
+export async function depositCancelOrder(data) {
+	try {
+		const response = await client.post(`${urls.depositCancelOrder}`, data)
+		return response.data
+	} catch (e) {
+		throw new Error('입금 취소 중 오류가 발생했습니다.')
+	}
+}
+
+// 전체 입금 취소
+export async function depositCancelOrderList(data) {
+	try {
+		const response = await client.post(`${urls.depositCancelOrderList}`, data)
+		return response.data
+	} catch (e) {
+		throw new Error('입금 취소 중 오류가 발생했습니다.')
+	}
 }
