7846a055f890ea02ce928886cabbba758d28ad1c
Jin Park <jinpark1dev@gmail.com>
2024-01-30 18:13:58 +0900

    feature: 상시 판매 패키지 > 패키지번호 > 상세 (진행중)

--- src/constants/admin/packageProducts.js
+++ src/constants/admin/packageProducts.js
@@ -1,4 +1,6 @@
 // 상시 판매 관리 > 패캐지
+import MarkerCellRenderer from '../../pages/Table/MarkerCellRenderer'
+import { getNormalTableRows } from '../../utils/table'
 
 var checkboxSelection = function (params) {
 	// we put checkbox on the name if we are not doing grouping
@@ -10,26 +12,33 @@ var headerCheckboxSelection = function (params) {
 	return params.columnApi.getRowGroupColumns().length === 0
 }
 
-export const packageFieldsCols = [
-	{ field: '', maxWidth: 50, checkboxSelection: checkboxSelection, headerCheckboxSelection: headerCheckboxSelection },
-	{ field: '순번', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '노출상태', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '패키지명', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '패키지번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '상시판매 번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '상시판매가', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '상시판매 상태', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '상시판매 주문일자', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '경매번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '판매구분', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '판매유형', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '경매시작단가', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '중량', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '메모', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '비고', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '최종수정자', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-	{ field: '최종수정일시', minWidth: 100, cellStyle: { 'text-align': 'center' } },
-]
+export const packageFieldsCols = (numberClickHandler = undefined) =>
+	getNormalTableRows([
+		{ field: '', maxWidth: 50, checkboxSelection: checkboxSelection, headerCheckboxSelection: headerCheckboxSelection },
+		{ field: '순번', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '노출상태', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '패키지명', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{
+			field: '패키지번호',
+			minWidth: 100,
+			cellRenderer: MarkerCellRenderer,
+			valueGetter: (v) => ({ ...v.data[v.column.colId], clickHandler: numberClickHandler }),
+		},
+		// { field: '패키지번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '상시판매 번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '상시판매가', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '상시판매 상태', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '상시판매 주문일자', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '경매번호', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '판매구분', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '판매유형', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '경매시작단가', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '중량', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '메모', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '비고', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '최종수정자', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+		{ field: '최종수정일시', minWidth: 100, cellStyle: { 'text-align': 'center' } },
+	])
 
 /*
     api/package-product
