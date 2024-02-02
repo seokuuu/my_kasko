b33d8153cb1c18e5f305a4c6813e9dae387b639e
KimHyuk97 <wgilooy97@naver.com>
2024-02-02 11:27:02 +0900

    actions test

--- src/pages/Main/MainPage.jsx
+++ src/pages/Main/MainPage.jsx
@@ -5,12 +5,10 @@ import Header from '../../components/Header/Header'
 import SubHeader from '../../components/Header/SubHeader'
 
 import { useState } from 'react'
-import useAlert from '../../store/Alert/useAlert'
 
 const MainPage = () => {
+	const depth2Color = ''
 	const [expanded, setExpanded] = useState('')
-	const [depth2Color, setDepth2Color] = useState('')
-	const { simpleAlert, showAlert, simpleConfirm, showConfirm, redAlert } = useAlert()
 
 	return (
 		<>
@@ -21,17 +19,6 @@ const MainPage = () => {
 					<SubHeader />
 					<OverAllTable>
 						<div>메인 페이지입니다</div>
-						<div onClick={() => simpleAlert('안녕하세요.')}>심플얼렛</div>
-						<div onClick={() => showAlert({ title: '안녕하세요', content: '안녕', func: () => console.log('alert') })}>
-							Alert테스트
-						</div>
-						<div onClick={() => simpleConfirm('안녕하세요', () => console.log('심플confirm'))}>심플Confirm</div>
-						<div
-							onClick={() => showConfirm({ title: '안녕하세요', content: '안녕', func: () => console.log('confirm') })}
-						>
-							Confirm테스트
-						</div>
-						<div onClick={() => redAlert('안녕', () => console.log('rede'))}>레드 버튼</div>
 					</OverAllTable>
 				</OverAllSub>
 			</OverAllMain>
