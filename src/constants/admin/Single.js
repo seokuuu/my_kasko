3b0ee8985eaf14cc6c127909432713d44b1be202
dbsghqls <dbsghqls>
2024-02-13 08:27:31 +0900

    fix: 카스코철강 명칭 변경

--- src/constants/admin/Single.js
+++ src/constants/admin/Single.js
@@ -2,275 +2,261 @@
 import RecommendCellRenderer from '../../pages/Table/RecommendCellRenderer'
 import RecommendCellRenderer2 from '../../pages/Table/RecommendCellRenderer2'
 import BtnCellRenderer from '../../pages/Table/BtnCellRenderer.jsx'
-import InputCellRenderer from "../../pages/Table/InputCellRenderer.jsx"
+import InputCellRenderer from '../../pages/Table/InputCellRenderer.jsx'
 import { FieldsSettings } from '../../utils/fieldsSetting.js'
 import { commonStyles } from '../commonCellStyle.js'
 import { recommendCell } from '../../pages/Product/SingleProduct/cellRender/RecommendCellRender.jsx'
 
 var checkboxSelection = function (params) {
-  // we put checkbox on the name if we are not doing grouping
-  return params.columnApi.getRowGroupColumns().length === 0
+	// we put checkbox on the name if we are not doing grouping
+	return params.columnApi.getRowGroupColumns().length === 0
 }
 
-var headerCheckboxSelection = function(params) {
-  // we put checkbox on the name if we are not doing grouping
-  return params.columnApi.getRowGroupColumns().length === 0
+var headerCheckboxSelection = function (params) {
+	// we put checkbox on the name if we are not doing grouping
+	return params.columnApi.getRowGroupColumns().length === 0
 }
 
-export const singleDispatchFields= {
-  '순번':'index',
-  '고유 번호': 'uid',
-  '제품 번호': 'number',
-  '규격 약호': 'spec',
-  '창고': 'storageName',
-  '제품 사양': 'wdh',
-  '두께': 'thickness',
-  '폭': 'width',
-  '길이': 'length',
-  '중량': 'weight',
-  '제품 등급': 'grade',
-  '용도 코드': 'usageCode',
-  '용도명': 'usageCodeName',
-  '경매 등록 상태': 'registrationStatus',
-  '아울렛 상태':'outletStatus',
-  '아울렛 가격':'outletPrice',
-    'C%': 'c',
-    'Si': 'si',
-    'Mn': 'mn',
-    'P': 'p',
-    'S': 's',
-    'TS': 'ts',
-    'YP': 'yp',
-    'EL': 'el',
-    '제품군명': 'spart',
-    '제품명': 'productName',
-    '정척 여부': 'preferThickness',
-    '여재 원인 코드': 'causeCode',
-    '여재 원인명': 'causeCodeName',
-    '입고일': 'receiptDate',
-    '유찰 횟수': 'failCount',
-    '제품 상태': 'productStatus',
-    '매입처': 'supplier',
-    '제조사': 'maker',
-    '판매 구분': 'saleCategory',
-    '판매 제외 사유': 'excludeSaleReason',
-    '재고 상태': 'stockStatus',
-    '판매 유형': 'saleType',
-    '입고 상태': 'receiptStatus',
-    '상시 판매 상태': 'saleStatus',
-    '판매가 유형': 'salePriceType',
-    '추천 제품 여부': 'bestStatus',
-    '등록 일자': 'createDate',
-    '최종 수정 일자': 'updateDate',
-    '최종 수정자': 'lastedUpdater',
-    '매입가': 'price',
-    '노출 상태': 'viewStatus',
-    '경매 시작 단가': 'auctionStartPrice',
-    '주문 상태': 'orderStatus',
-    '클레임 진행 상태': 'claimStatus',
-    '확정 전송 일자': 'sendDate',
-    '패키지명': 'packageName',
-    '패키지번호': 'packageNumber',
-    'Pro.No': 'productNoNumber',
-    '경매 번호': 'auctionNumber',
-    '제품 낙찰 단가': 'productBiddingPrice',
-    '낙찰 총 단가': 'totalBiddingPrice',
-    '기본 운임 단가': 'freightFee',
-    '할증 운임 단가': 'extraUnitPrice',
-    '제품 공급가': 'orderPrice',
-    '제품 부가세': 'orderPriceVat',
-    '운반비 공급가': 'freightCost',
-    '운반비 부가세': 'freightCostVat',
-    '목적지명': 'destinationName',
-    '낙찰 상태': 'biddingStatus',
-    '메모': 'memo',
-    '비고': 'note',
-    '상시 판매가': 'salePrice'
-  }
-  
+export const singleDispatchFields = {
+	순번: 'index',
+	'고유 번호': 'uid',
+	'제품 번호': 'number',
+	'규격 약호': 'spec',
+	창고: 'storageName',
+	'제품 사양': 'wdh',
+	두께: 'thickness',
+	폭: 'width',
+	길이: 'length',
+	중량: 'weight',
+	'제품 등급': 'grade',
+	'용도 코드': 'usageCode',
+	용도명: 'usageCodeName',
+	'경매 등록 상태': 'registrationStatus',
+	'아울렛 상태': 'outletStatus',
+	'아울렛 가격': 'outletPrice',
+	'C%': 'c',
+	Si: 'si',
+	Mn: 'mn',
+	P: 'p',
+	S: 's',
+	TS: 'ts',
+	YP: 'yp',
+	EL: 'el',
+	제품군명: 'spart',
+	제품명: 'productName',
+	'정척 여부': 'preferThickness',
+	'여재 원인 코드': 'causeCode',
+	'여재 원인명': 'causeCodeName',
+	입고일: 'receiptDate',
+	'유찰 횟수': 'failCount',
+	'제품 상태': 'productStatus',
+	매입처: 'supplier',
+	제조사: 'maker',
+	'판매 구분': 'saleCategory',
+	'판매 제외 사유': 'excludeSaleReason',
+	'재고 상태': 'stockStatus',
+	'판매 유형': 'saleType',
+	'입고 상태': 'receiptStatus',
+	'상시 판매 상태': 'saleStatus',
+	'판매가 유형': 'salePriceType',
+	'추천 제품 여부': 'bestStatus',
+	'등록 일자': 'createDate',
+	'최종 수정 일자': 'updateDate',
+	'최종 수정자': 'lastedUpdater',
+	매입가: 'price',
+	'노출 상태': 'viewStatus',
+	'경매 시작 단가': 'auctionStartPrice',
+	'주문 상태': 'orderStatus',
+	'클레임 진행 상태': 'claimStatus',
+	'확정 전송 일자': 'sendDate',
+	패키지명: 'packageName',
+	패키지번호: 'packageNumber',
+	'Pro.No': 'productNoNumber',
+	'경매 번호': 'auctionNumber',
+	'제품 낙찰 단가': 'productBiddingPrice',
+	'낙찰 총 단가': 'totalBiddingPrice',
+	'기본 운임 단가': 'freightFee',
+	'할증 운임 단가': 'extraUnitPrice',
+	'제품 공급가': 'orderPrice',
+	'제품 부가세': 'orderPriceVat',
+	'운반비 공급가': 'freightCost',
+	'운반비 부가세': 'freightCostVat',
+	목적지명: 'destinationName',
+	'낙찰 상태': 'biddingStatus',
+	메모: 'memo',
+	비고: 'note',
+	'상시 판매가': 'salePrice',
+}
 
-export const SingleDispatchFieldsCols= [
-  { field: '',  ...commonStyles, minWidth: 50, checkboxSelection, headerCheckboxSelection },
-  { ...commonStyles,
-    minWidth: 100,  
-    cellRenderer: BtnCellRenderer,
-    cellRendererParams: {
-      uidFieldName: '경매 번호',
-      editType: 'productModify',
-    }
-  },
-  { ...commonStyles,
-    field:'순번',
-    minWidth: 100,  
-  },
-  { ...commonStyles,
-    field:'고유 번호',
-    minWidth: 100,  
-  },
+export const SingleDispatchFieldsCols = [
+	{ field: '', ...commonStyles, minWidth: 50, checkboxSelection, headerCheckboxSelection },
+	{
+		...commonStyles,
+		minWidth: 100,
+		cellRenderer: BtnCellRenderer,
+		cellRendererParams: {
+			uidFieldName: '경매 번호',
+			editType: 'productModify',
+		},
+	},
+	{ ...commonStyles, field: '순번', minWidth: 100 },
+	{ ...commonStyles, field: '고유 번호', minWidth: 100 },
 
-  { ...commonStyles,
-    field:'제품 번호',
-    minWidth: 200,  
-    cellRenderer:recommendCell,
-   
-  },
-    ...Object.keys(singleDispatchFields).slice(3).map((item) => ({
-    ...commonStyles,
-    field: item,
-    editable:(item === '메모' || item === '비고' )? true:false,
-    minWidth: (item === '메모' || item === '비고' )? 200 : 150,
-    cellRenderer:(params) => {
-      if(item === '메모' || item === '비고'){
-        return params.value
-      }
-      if(typeof params.value === 'boolean'){
-        return params.value ? 'Y' : 'N';
-      }else{
-        return params.value
-      }
-    },
-  })), 
+	{ ...commonStyles, field: '제품 번호', minWidth: 200, cellRenderer: recommendCell },
+	...Object.keys(singleDispatchFields)
+		.slice(3)
+		.map((item) => ({
+			...commonStyles,
+			field: item,
+			editable: item === '메모' || item === '비고' ? true : false,
+			minWidth: item === '메모' || item === '비고' ? 200 : 150,
+			cellRenderer: (params) => {
+				if (item === '메모' || item === '비고') {
+					return params.value
+				}
+				if (typeof params.value === 'boolean') {
+					return params.value ? 'Y' : 'N'
+				} else {
+					return params.value
+				}
+			},
+		})),
 ]
 
-export const SingleSalesDispatchFieldsCols= [
-  { field: '',  ...commonStyles, minWidth: 50, checkboxSelection, headerCheckboxSelection },
-  { ...commonStyles,
-    minWidth: 100,  
-    cellRenderer: BtnCellRenderer,
-    cellRendererParams: {
-      uidFieldName: '경매 번호',
-      editType: 'productModify',
-    }
-  },
-  { ...commonStyles,
-    field:'순번',
-    minWidth: 100,  
-  },
-  { ...commonStyles,
-    field:'고유 번호',
-    minWidth: 100,  
-  },
+export const SingleSalesDispatchFieldsCols = [
+	{ field: '', ...commonStyles, minWidth: 50, checkboxSelection, headerCheckboxSelection },
+	{
+		...commonStyles,
+		minWidth: 100,
+		cellRenderer: BtnCellRenderer,
+		cellRendererParams: {
+			uidFieldName: '경매 번호',
+			editType: 'productModify',
+		},
+	},
+	{ ...commonStyles, field: '순번', minWidth: 100 },
+	{ ...commonStyles, field: '고유 번호', minWidth: 100 },
 
-  { ...commonStyles,
-    field:'제품 번호',
-    minWidth: 200,  
-    cellRenderer:recommendCell,
-   
-  },
-    ...Object.keys(singleDispatchFields).slice(3).map((item) => ({
-    ...commonStyles,
-    field: item,
-    editable:(item === '메모' || item === '비고' )? true:false,
-    minWidth: (item === '메모' || item === '비고' )? 200 : 150,
-    cellRenderer:(params) => {
-      if(item === '메모' || item === '비고'){
-        return params.value
-      }
-      if(typeof params.value === 'boolean'){
-        return params.value ? 'Y' : 'N';
-      }else{
-        return params.value
-      }
-    },
-  })), 
+	{ ...commonStyles, field: '제품 번호', minWidth: 200, cellRenderer: recommendCell },
+	...Object.keys(singleDispatchFields)
+		.slice(3)
+		.map((item) => ({
+			...commonStyles,
+			field: item,
+			editable: item === '메모' || item === '비고' ? true : false,
+			minWidth: item === '메모' || item === '비고' ? 200 : 150,
+			cellRenderer: (params) => {
+				if (item === '메모' || item === '비고') {
+					return params.value
+				}
+				if (typeof params.value === 'boolean') {
+					return params.value ? 'Y' : 'N'
+				} else {
+					return params.value
+				}
+			},
+		})),
 ]
-var optionFn = function(key) {
+var optionFn = function (key) {
+	let options = []
 
-  let options = []
+	switch (key) {
+		case '재고 상태':
+			options = ['자사 재고', '타사 재고']
+			break
+		case '제조사':
+			options = ['현대제철', '동은스틸']
+			break
+		case '판매 구분':
+			options = ['판매재', '판매 제외재', '판매 완료재']
+			break
+		case '매입처':
+			options = ['현대제철', '카스코철강']
+			break
+		default:
+			break
+	}
 
-  switch (key) {
-    case '재고 상태':
-      options = ['자사 재고', '타사 재고']
-      break
-    case '제조사':
-      options = ['현대제철', '동은스틸']
-      break
-    case '판매 구분':
-      options = ['판매재', '판매 제외재', '판매 완료재']
-      break
-    case '매입처':
-      options = ['현대제철', '카스코 철강']
-      break
-    default:
-      break
-  }
-
-  return options
+	return options
 }
 /// 제품 수정 창에 데려올 필드와 설정하는 컬럼
 
 export const SingleModifyFields = {
-  '제품 번호': 'number',
-  '저장 위치': 'storage',
-  '저장 위치명': 'storageName',
-  '규격 약호': 'spec',
-  '제품 사양': 'wdh',
-  '두께': 'thickness',
-  '폭': 'width',
-  '길이': 'length',
-  '중량': 'weight',
-  '등급': 'grade',
-  '용도 코드': 'usageCode',
-  '용도명': 'usageCodeName',
-  'C%': 'c',
-  'Si': 'si',
-  'Mn': 'mn',
-  'P': 'p',
-  'S': 's',
-  'TS': 'ts',
-  'YP': 'yp',
-  'EL': 'el',
-  '제품군 코드': 'spartCode',
-  '제품군명': 'spart',
-  '매입처': 'supplier',
-  '제조사': 'maker',
-  '품명': 'name',
-  매입가: 'price',
-  '정척 여부': 'preferThickness',
-  '여재 원인 코드': 'causeCode',
-  '여재 원인명': 'causeCodeName',
-  '입고일': 'receiptDate',
-  '재고 상태': 'stockStatus',
-  '판매 구분': 'saleCategory',
+	'제품 번호': 'number',
+	'저장 위치': 'storage',
+	'저장 위치명': 'storageName',
+	'규격 약호': 'spec',
+	'제품 사양': 'wdh',
+	두께: 'thickness',
+	폭: 'width',
+	길이: 'length',
+	중량: 'weight',
+	등급: 'grade',
+	'용도 코드': 'usageCode',
+	용도명: 'usageCodeName',
+	'C%': 'c',
+	Si: 'si',
+	Mn: 'mn',
+	P: 'p',
+	S: 's',
+	TS: 'ts',
+	YP: 'yp',
+	EL: 'el',
+	'제품군 코드': 'spartCode',
+	제품군명: 'spart',
+	매입처: 'supplier',
+	제조사: 'maker',
+	품명: 'name',
+	매입가: 'price',
+	'정척 여부': 'preferThickness',
+	'여재 원인 코드': 'causeCode',
+	'여재 원인명': 'causeCodeName',
+	입고일: 'receiptDate',
+	'재고 상태': 'stockStatus',
+	'판매 구분': 'saleCategory',
 }
 
-
 export const SingleModifyDispatchFieldsCols = [
-  {
-    field: '', headerClass: 'custom-header-style', flex: 1,
-    cellStyle: { borderRight: '1px solid #c8c8c8', textAlign: 'center' },
-     minWidth: 50, checkboxSelection, headerCheckboxSelection },
-  ...Object.entries(SingleModifyFields).map(([k,v]) => {
-    if(k !=='재고 상태' && k !== '판매 구분' && k !== '매입처' && k !=='제조사' ){
-      return {
-        ...commonStyles,
-        field: k,
-        minWidth: 150,
-        editable:true,
-        cellRenderer: InputCellRenderer,
-        cellRendererParams:{
-          uidFieldName:k,
-          valueName:v,
-          type:'input'
-        }
-      }
-    } else {
-      return {
-        ...commonStyles,
-        field: k,
-        minWidth: 150,
-        editable:true,
-        cellEditor:'agSelectCellEditor',
-        cellRenderer:InputCellRenderer,
-        cellRendererParams:{
-          uidFieldName:k,
-          valueName:v,
-          type:'select'
-        },
-        cellEditorParams: {
-          values: optionFn(k), // 셀렉트 박스에 표시할 옵션들
-        },
-      }
-    }
-  }),
+	{
+		field: '',
+		headerClass: 'custom-header-style',
+		flex: 1,
+		cellStyle: { borderRight: '1px solid #c8c8c8', textAlign: 'center' },
+		minWidth: 50,
+		checkboxSelection,
+		headerCheckboxSelection,
+	},
+	...Object.entries(SingleModifyFields).map(([k, v]) => {
+		if (k !== '재고 상태' && k !== '판매 구분' && k !== '매입처' && k !== '제조사') {
+			return {
+				...commonStyles,
+				field: k,
+				minWidth: 150,
+				editable: true,
+				cellRenderer: InputCellRenderer,
+				cellRendererParams: {
+					uidFieldName: k,
+					valueName: v,
+					type: 'input',
+				},
+			}
+		} else {
+			return {
+				...commonStyles,
+				field: k,
+				minWidth: 150,
+				editable: true,
+				cellEditor: 'agSelectCellEditor',
+				cellRenderer: InputCellRenderer,
+				cellRendererParams: {
+					uidFieldName: k,
+					valueName: v,
+					type: 'select',
+				},
+				cellEditorParams: {
+					values: optionFn(k), // 셀렉트 박스에 표시할 옵션들
+				},
+			}
+		}
+	}),
 ]
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,98 +7,98 @@ import { SkyBtn, WhiteSkyBtn } from '../common/Button/Button'
 import { Link, useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
-  const [showHeader, setShowHeader] = useAtom(headerAtom)
-  const [showAccordion, setShowAccordion] = useAtom(accordionAtom)
-  const [showSubHeader, setShowSubHeader] = useAtom(subHeaderAtom)
-  setShowHeader(false)
-  setShowAccordion(false)
-  setShowSubHeader(false)
-  const navigate = useNavigate()
+	const [showHeader, setShowHeader] = useAtom(headerAtom)
+	const [showAccordion, setShowAccordion] = useAtom(accordionAtom)
+	const [showSubHeader, setShowSubHeader] = useAtom(subHeaderAtom)
+	setShowHeader(false)
+	setShowAccordion(false)
+	setShowSubHeader(false)
+	const navigate = useNavigate()
 
-  const goToHome = () => {
-    startTransition(() => {
-      navigate('/')
-    })
-  }
+	const goToHome = () => {
+		startTransition(() => {
+			navigate('/')
+		})
+	}
 
-  return (
-    <Wrap>
-      <NotFoundWrap>
-        <Main>
-          <div>
-            <div style={{ fontSize: '64px', fontWeight: '900', marginBottom: '10px' }}>404</div>
-            <div style={{ fontSize: '23px', marginBottom: '15px' }}>페이지를 찾을 수 없습니다.</div>
-            <div style={{ marginTop: '10px' }}>
-              <p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
-              <p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
-            </div>
-            <BtnWrap>
-              <SkyBtn
-                style={{ color: '#4C83D6', background: 'white', border: '1px solid #4C83D6' }}
-                width={35}
-                height={40}
-                fontSize={18}
-                onClick={() => {
-                  navigate(-1)
-                }}
-              >
-                이전페이지
-              </SkyBtn>
-              <SkyBtn onClick={goToHome} width={35} height={40} fontSize={18}>
-                카스코 철강 홈
-              </SkyBtn>
-            </BtnWrap>
-          </div>
-        </Main>
-      </NotFoundWrap>
-    </Wrap>
-  )
+	return (
+		<Wrap>
+			<NotFoundWrap>
+				<Main>
+					<div>
+						<div style={{ fontSize: '64px', fontWeight: '900', marginBottom: '10px' }}>404</div>
+						<div style={{ fontSize: '23px', marginBottom: '15px' }}>페이지를 찾을 수 없습니다.</div>
+						<div style={{ marginTop: '10px' }}>
+							<p>페이지가 존재하지 않거나, 사용할 수 없는 페이지입니다.</p>
+							<p>입력하신 주소가 정확한지 다시 한 번 확인해주세요.</p>
+						</div>
+						<BtnWrap>
+							<SkyBtn
+								style={{ color: '#4C83D6', background: 'white', border: '1px solid #4C83D6' }}
+								width={35}
+								height={40}
+								fontSize={18}
+								onClick={() => {
+									navigate(-1)
+								}}
+							>
+								이전페이지
+							</SkyBtn>
+							<SkyBtn onClick={goToHome} width={35} height={40} fontSize={18}>
+								카스코철강 홈
+							</SkyBtn>
+						</BtnWrap>
+					</div>
+				</Main>
+			</NotFoundWrap>
+		</Wrap>
+	)
 }
 
 export default NotFound
 
 const Wrap = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  background-color: #eef3fb;
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	background-color: #eef3fb;
 `
 
 const NotFoundWrap = styled.div`
-  width: 890px;
-  height: 480px;
-  margin: auto;
+	width: 890px;
+	height: 480px;
+	margin: auto;
 
-  display: flex;
-  background-color: white;
+	display: flex;
+	background-color: white;
 `
 const Main = styled.div`
-  width: 65%;
-  height: 50%;
-  margin: auto;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  display: flex;
+	width: 65%;
+	height: 50%;
+	margin: auto;
+	text-align: center;
+	justify-content: center;
+	align-items: center;
+	display: flex;
 
-  > div {
-    margin: auto;
-  }
+	> div {
+		margin: auto;
+	}
 
-  p {
-    font-size: 17px;
-    font-weight: lighter;
-  }
+	p {
+		font-size: 17px;
+		font-weight: lighter;
+	}
 `
 
 const BtnWrap = styled.div`
-  margin-left: auto;
-  margin-right: auto;
-  width: 450px;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-  margin-top: 30px;
+	margin-left: auto;
+	margin-right: auto;
+	width: 450px;
+	height: 50px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
+	margin-top: 30px;
 `
