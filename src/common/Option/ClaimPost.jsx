fe169106ded0aec8945d6a9940fc3bc2d4808ce3
dbsghqls <dbsghqls>
2024-02-13 14:28:03 +0900

    fix: 클레임 페이지 수정

--- src/common/Option/ClaimPost.jsx
+++ src/common/Option/ClaimPost.jsx
@@ -5,19 +5,19 @@ import { styled } from 'styled-components'
 
 //창고 구분
 export const claimOngoingStatus = [
-  { value: 'ask0', label: '진행중 ' },
-  { value: 'ask1', label: '종료' },
-  { value: 'ask2', label: '반품' },
-  { value: 'ask3', label: '교정' },
-  // { value: 'ask4', label: '현금 보전' },
-  { value: 'ask4', label: '보상' },
+	{ value: 'ask0', label: '진행중 ' },
+	{ value: 'ask1', label: '종료' },
+	{ value: 'ask2', label: '반품' },
+	{ value: 'ask3', label: '교정' },
+	{ value: 'ask4', label: '보상' },
+	{ value: 'ask5', label: '현금 보전' },
 ]
 
 export const ClaimSelect = styled(Select)`
-  display: block;
-  position: relative;
-  left: -25px;
-  min-width: 200px;
-  line-height: 20px;
-  text-align: center;
+	display: block;
+	position: relative;
+	left: -25px;
+	min-width: 200px;
+	line-height: 20px;
+	text-align: center;
 `
