6305402c69e3df35c8d68ea8d36a2e79a2b531f9
dbsghqls <dbsghqls>
2024-02-22 15:13:06 +0900

    fix: 고객센터 / 마이페이지 / 공지 / 자료실 항목 정리

--- src/pages/Table/FixCellRenderer.jsx
+++ src/pages/Table/FixCellRenderer.jsx
@@ -1,21 +1,23 @@
 import styled from 'styled-components'
 
 export default function FixCellRenderer(props) {
-  return <FixCell cellValue={props.value}>{props.value == '고정' ? '고정' : props.value}</FixCell>
+	return <FixCell cellValue={props.value}>{props.value == '고정' ? '고정' : props.value}</FixCell>
 }
 
 const FixCell = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 12px;
-  padding: 4px;
-  margin-top: 4px;
-  width: 34px;
-  /* padding: 4px; */
-  height: 30px;
-  font-weight: ${(props) => (props.cellValue === '고정' ? 'bold' : '')};
-  color: ${(props) => (props.cellValue === '고정' ? 'white' : 'black')};
-  background-color: ${(props) => (props.cellValue === '고정' ? '#B02525' : '')};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	font-size: 12px;
+	padding: 4px;
+	margin-top: 4px;
+	margin-left: auto;
+	margin-right: auto;
+	width: 34px;
+	/* padding: 4px; */
+	height: 30px;
+	font-weight: ${(props) => (props.cellValue === '고정' ? 'bold' : '')};
+	color: ${(props) => (props.cellValue === '고정' ? 'white' : 'black')};
+	background-color: ${(props) => (props.cellValue === '고정' ? '#B02525' : '')};
 `
