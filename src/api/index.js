6f11f88031344d304501a3ae4aa545e4dc502a6f
KimHyuk97 <wgilooy97@naver.com>
2024-03-07 17:19:49 +0900

    240307 axios 타임아웃

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ export const formHeaders = { 'Content-Type': 'multipart/form-data' }
 
 export const client = axios.create({
 	baseURL: API_PATH,
-	timeout: 15000,
+	timeout: 30000,
 	headers: {
 		'Cache-Control': 'no-cache',
 		'Content-Type': 'application/json',
