bacc42796076e015bed755ec3da4d71c920dcb1b
Seok <wazzagoo10@gmail.com>
2024-04-04 21:24:16 +0900

    [fix] : 경매 패키지 오류

--- src/utils/KilogramSum.js
+++ src/utils/KilogramSum.js
@@ -4,10 +4,10 @@ export function KilogramSum(data, name) {
 		data.forEach((i) => {
 			if (name) {
 				sum += Number(i[name])
-			} else if (i?.중량.includes(',')) {
+			} else if (i?.중량?.includes(',')) {
 				sum += Number(i.중량.replace(/,/g, ''))
-			} else if (i[name].includes(',')) {
-				sum += Number(i[name].replace(/,/g, ''))
+			} else if (i[name]?.includes(',')) {
+				sum += Number(i[name]?.replace(/,/g, ''))
 			} else {
 				sum += Number(i.중량)
 			}
