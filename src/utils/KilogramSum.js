f35148866bcccbb271c26b0398108341001cbb76
Seok <wazzagoo10@gmail.com>
2024-04-04 17:16:33 +0900

    [fix] : 선택 중량 콤마 예외 처리

--- src/utils/KilogramSum.js
+++ src/utils/KilogramSum.js
@@ -1,10 +1,13 @@
-export function KilogramSum(data,name) {
+export function KilogramSum(data, name) {
 	let sum = 0
 	if (Array.isArray(data)) {
 		data.forEach((i) => {
-			if(name){
+			console.log('씨발', i?.중량)
+			if (name) {
 				sum += Number(i[name])
-			} else{
+			} else if (i?.중량.includes(',')) {
+				sum += Number(i.중량.replace(/,/g, ''))
+			} else {
 				sum += Number(i.중량)
 			}
 		})
