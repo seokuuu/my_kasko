da822100b600648294924bd40aa8bbad52e93f33
Seok <wazzagoo10@gmail.com>
2024-04-16 12:03:13 +0900

    [fix] : 운영 관리 오류 수정

--- src/pages/Operate/Common/FAQ/FAQPostPage.jsx
+++ src/pages/Operate/Common/FAQ/FAQPostPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { OverAllMain, OverAllSub } from '../../../../common/Overall/Overall.styled'
 
@@ -10,20 +10,22 @@ import { useParams } from 'react-router-dom'
 import FAQPost from './FAQPost'
 
 const FAQPostPage = () => {
-  const { id } = useParams()
+	const { id } = useParams()
+	const [expanded, setExpanded] = useState('운영 관리')
+	const [depth2Color, setDepth2Color] = useState('일반 관리')
 
-  return (
-    <>
-      <Header />
-      <OverAllMain>
-        <SideBar />
-        <OverAllSub>
-          <SubHeader />
-          <FAQPost detailsId={id} />
-        </OverAllSub>
-      </OverAllMain>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<OverAllMain>
+				<SideBar expanded={expanded} setExpanded={setExpanded} depth2Color={depth2Color} />
+				<OverAllSub>
+					<SubHeader />
+					<FAQPost detailsId={id} />
+				</OverAllSub>
+			</OverAllMain>
+		</>
+	)
 }
 
 export default FAQPostPage
