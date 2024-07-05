0e53d77bb160bc10fc3fe76c1328a5091f6f668b
KimHyuk97 <wgilooy97@naver.com>
2024-07-06 04:39:42 +0900

    - 출고현황 변경사항 수정

--- src/pages/Shipping/Request/RequestRecom.jsx
+++ src/pages/Shipping/Request/RequestRecom.jsx
@@ -101,7 +101,9 @@ const RequestRecom = () => {
 		if (orderUids.length === 0) {
 			return simpleAlert('선별 목록에 제품을 추가해주세요.')
 		}
-		simpleConfirm('선별 등록하시겠습니까?', () => onCreateMerge({ dockStatus, orderUids }))
+		simpleConfirm(`${auth.role === '카스코철강' ? '출하 지시하시겠습니까?' : '선별 등록 하시겠습니까?'}`, () =>
+			onCreateMerge({ dockStatus, orderUids }),
+		)
 	}
 
 	useEffect(() => {
@@ -139,7 +141,7 @@ const RequestRecom = () => {
 					</div>
 					<div style={{ display: 'flex', gap: '10px' }}>
 						<WhiteRedBtn onClick={onListRemove}>목록 제거</WhiteRedBtn>
-						<WhiteSkyBtn onClick={onRegister}>{auth?.role === '카스코철강' ? '선별 등록' : '출하 지시'}</WhiteSkyBtn>
+						<WhiteSkyBtn onClick={onRegister}>{auth?.role === '카스코철강' ? '출하 지시' : '선별 등록'}</WhiteSkyBtn>
 					</div>
 				</TCSubContainer>
 				<TableV2 getRow={tableRowData} loading={isLoading} getCol={RegisterFieldsCols(RegisterFields(auth))} />
